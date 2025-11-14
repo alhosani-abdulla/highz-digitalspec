4162c9d9856428d531e65971a5cd80213c6fd71f
alhosani-abdulla <ama1029@nyu.edu>
2025-11-13 20:16:08 -0500

    Add convenient aliases for spectrometer control
    - TakeSpecs: Run spectrometer via pipenv
    - GetSpecs: Navigate to data storage directory
    - SpecShell: Enter spectrometer virtual environment
    - TakeSpecsLauncher: Run spectrometer with launcher script

---
+++ scripts/highz_alias.sh
@@ -0,0 +1,13 @@
+# Custom aliases for HighZ Digital Spectrometer Control
+
+# Run the spectrometer using pipenv
+alias TakeSpecs='cd ~/highz-digitalspec && pipenv run python src/run_spectrometer.py'
+
+# Navigate to the data storage directory (update path as needed)
+alias GetSpecs='cd /home/peterson/spectrometer_data'
+
+# Quick way to enter the spectrometer virtual environment
+alias SpecShell='cd ~/highz-digitalspec && pipenv shell'
+
+# Run the spectrometer with the launcher script
+alias TakeSpecsLauncher='~/highz-digitalspec/scripts/launcher.sh'
